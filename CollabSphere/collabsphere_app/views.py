08a87bb868a1b08a4e9cded2ad3455ba0345c8d9
dont @ me <ninaisabelletupas@gmail.com>
2025-09-23 12:04:40 +0800

    feat(loginh): add login view and template, update urls and initial setup

--- CollabSphere/collabsphere_app/views.py
+++ CollabSphere/collabsphere_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, welcome to CollabSphere!")
+   return render(request, "login.html")
