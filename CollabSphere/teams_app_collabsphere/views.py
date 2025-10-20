db3edcfa6261b061670a7b4e90f818d4e79badd4
dont @ me <ninaisabelletupas@gmail.com>
2025-10-20 10:07:09 +0800

    feat(teams): inital css and ui for teams app

--- CollabSphere/teams_app_collabsphere/views.py
+++ CollabSphere/teams_app_collabsphere/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def teams(request):
+    if not request.user.is_authenticated:
+        messages.warning(request, "You must be logged in to view this page.")
+        return redirect("login")  
+    
+    return render(request, "teams.html")
