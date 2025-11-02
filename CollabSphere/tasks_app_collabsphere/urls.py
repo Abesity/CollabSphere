337e778a16e2b6096802c76c3f834ff0943cdf8a
John Fil Roe Taboada <186065733+Taboada40@users.noreply.github.com>
2025-11-02 10:02:15 +0800

    Remove View Details button from notifications

--- CollabSphere/tasks_app_collabsphere/urls.py
+++ CollabSphere/tasks_app_collabsphere/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.tasks, name="tasks"),  # Loads the task creation modal
     path("create/", views.task_create, name="task_create"),
+    # Accept both /tasks/<id>/detail/ and /tasks/<id>/ to display the task detail modal
     path("<int:task_id>/detail/", views.task_detail, name="task_detail"),  # GET detail modal HTML
+    path("<int:task_id>/", views.task_detail, name="task_detail_short"),
     path("<int:task_id>/update/", views.task_update, name="task_update"),  # POST to update
     path("<int:task_id>/delete/", views.task_delete, name="task_delete"),  # POST to delete
     path('<int:task_id>/comment/', views.add_comment, name='add_comment'),  #for adding comments
