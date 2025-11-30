6bb22dc037fbec16212d464e44502ae37174fd1f
John Fil Roe Taboada <186065733+Taboada40@users.noreply.github.com>
2025-12-01 03:13:06 +0800

    Added category and added a professional design when clearing inbox

--- CollabSphere/notifications_app_collabsphere/urls.py
+++ CollabSphere/notifications_app_collabsphere/urls.py
@@ -6,6 +6,7 @@ app_name = 'notifications'
 urlpatterns = [
     path('', views.notifications_list, name='list'),
     path('mark-read/<int:notification_id>/', views.mark_notification_read, name='mark_read'),
+    path('delete/<int:notification_id>/', views.delete_notification, name='delete'),
     path('mark-all-read/', views.mark_all_read, name='mark_all_read'),
     path('clear-inbox/', views.clear_inbox, name='clear_inbox'),
     path('unread-count/', views.get_unread_count, name='unread_count'),
